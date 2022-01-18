e2c7347b0ff7da9b8bd504ee6c66e051973042de
Martin Hanicinec <hanicinecm@gmail.com>
2022-01-18 13:12:47 +0100

    revert back the last commit, it was nonsense

--- process.py
+++ process.py
@@ -2,14 +2,7 @@ import sys
 
 from exomol2lida.process_dataset import DatasetProcessor
 
-
-def main():
+if __name__ == "__main__":
     mol_formula = sys.argv[1]
     mol_processor = DatasetProcessor(mol_formula)
-    if "--print-def" in sys.argv:
-        print(mol_processor.molecule_input.def_parser.raw_text)
     mol_processor.process()
-
-
-if __name__ == "__main__":
-    main()
