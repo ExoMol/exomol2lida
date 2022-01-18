5a6346a65e7bd4bd7dc383b48db0006901189cb9
Martin Hanicinec <hanicinecm@gmail.com>
2022-01-18 13:08:48 +0100

    add an option to the process script to print out the .def file.

--- process.py
+++ process.py
@@ -6,6 +6,8 @@ from exomol2lida.process_dataset import DatasetProcessor
 def main():
     mol_formula = sys.argv[1]
     mol_processor = DatasetProcessor(mol_formula)
+    if "--print-def" in sys.argv:
+        print(mol_processor.molecule_input.def_parser.raw_text)
     mol_processor.process()
 
 
