cb3f90c22e80f892eea4566cd1a94bc77ced8242
Martin Hanicinec <hanicinecm@gmail.com>
2022-01-28 18:05:49 +0100

    minor tweak of the process.py script

--- process.py
+++ process.py
@@ -3,6 +3,7 @@ import sys
 from exomol2lida.process_dataset import DatasetProcessor
 
 if __name__ == "__main__":
+    include_original_lifetimes = "-tau" in sys.argv
     mol_formula = sys.argv[1]
     mol_processor = DatasetProcessor(mol_formula)
-    mol_processor.process()
+    mol_processor.process(include_original_lifetimes=include_original_lifetimes)
