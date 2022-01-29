e8730353771f2fe88d7c4a89f1776e1ae67303c5
Martin Hanicinec <hanicinecm@gmail.com>
2022-01-29 16:33:12 +0100

    tweak the top level process.py script

--- process.py
+++ process.py
@@ -2,8 +2,11 @@ import sys
 
 from exomol2lida.process_dataset import DatasetProcessor
 
-if __name__ == "__main__":
-    include_original_lifetimes = "-tau" in sys.argv
-    mol_formula = sys.argv[1]
+
+def process(mol_formula, include_original_lifetimes):
     mol_processor = DatasetProcessor(mol_formula)
     mol_processor.process(include_original_lifetimes=include_original_lifetimes)
+
+
+if __name__ == "__main__":
+    process(sys.argv[1], "-tau" in sys.argv)
